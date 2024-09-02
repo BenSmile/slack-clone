6126bcf5e8c5025752cbaa608fc702e8af86700a
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2024-09-02 21:29:18 +0100

    auth with google | github | password

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+import AuthScreen from '@/features/auth/components/auth-screen'
+import React from 'react'
+
+const SignInPage = () => {
+    return (
+        <AuthScreen />
+    )
+}
+
+export default SignInPage
